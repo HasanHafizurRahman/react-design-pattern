e19d7b2d5d8fbfdb54e0ef3f33c214756ad5edb7
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-07-17 16:46:01 +0600

    MovieTypes type alias

--- src/components/ListView.tsx
+++ src/components/ListView.tsx
@@ -1,13 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import React from "react";
+import { MovieTypes } from "../types/Movietypes";
 
-const ListView = ({ data }) => {
+interface ListViewProps {
+  data: MovieTypes[];
+}
+
+const ListView = ({ data }: ListViewProps) => {
   //   console.log("Data in ListView:", data);
   return (
     <div>
       <h1>List View</h1>
       {
         <ul>
-          {data.map((movie) => (
+          {data.map((movie: MovieTypes) => (
             <li key={movie.id}>
               <h2>{movie.title}</h2>
               <p>Director: {movie.director}</p>
---
+++ src/types/Movietypes.ts
@@ -0,0 +1,9 @@
+export type MovieTypes = {
+  id: number;
+  title: string;
+  director: string;
+  year: number;
+  rating: string;
+  runtime: number;
+  poster: string;
+};
